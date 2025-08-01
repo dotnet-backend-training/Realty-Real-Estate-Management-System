51fe96eedd3b23dc21a771e934b8a8e1304309f4
DiaaHSharqawi <diaasharqawi00@gmail.com>
2025-08-02 00:10:05 +0300

    docs: finalize and upload complete ERD diagram for database structure

--- Realty_Management_System_Domain/Entities/User.cs
+++ Realty_Management_System_Domain/Entities/User.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Realty_Management_System_Domain.Entities
 {
