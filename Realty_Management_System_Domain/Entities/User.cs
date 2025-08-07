0274a8dbe25aea896536a38d87679d37d43334d8
DiaaHSharqawi <diaasharqawi00@gmail.com>
2025-08-07 16:07:39 +0300

    feat(user): add profile image URL field to User model

--- Realty_Management_System_Domain/Entities/User.cs
+++ Realty_Management_System_Domain/Entities/User.cs
@@ -6,6 +6,7 @@ namespace Realty_Management_System_Domain.Entities
     {
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        public string ProfileImageUrl { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? UpdatedAt { get; set; }
         public Guid CountryId { get; set; }
